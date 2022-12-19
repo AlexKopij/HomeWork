80dd27c7d14ea5fbf25499840cc3028f41e5e80b
Aleksandr Kopij <akopij22097@gmail.com>
2022-12-19 21:31:26 +0300

    task 36

---
+++ HomeWork5/Task36/Program.cs
@@ -0,0 +1,65 @@
+﻿/*Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0*/
+
+//получить размерность массива с консоли
+int GetNumber(string message)
+{
+    int result;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;  
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Введите корректное число");
+        }
+    }
+    return result;
+}
+//получить заполненный массив рандомными числами
+int[] InitArray(int dimension)
+{
+    int[] array = new int[dimension];
+    Random rnd = new Random();
+
+    for (int i = 0; i < dimension; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
+
+    return array;
+}
+//распечатать массив на консоль
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+
+    Console.WriteLine();
+}
+
+int GetSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length;)
+    {
+        sum = sum + array[i];
+        i += 2;
+    }
+    return sum;
+}
+
+int dimension = GetNumber("Введите размерность массива:");
+int[] array = InitArray(dimension);
+PrintArray(array);
+int sum = GetSum(array);
+Console.WriteLine($"Сумма чисел, стоящих на нечетных позициях заданного массива, равна {sum}");
